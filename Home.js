299f515739f0e3d4f3eb6a3a35f845fbe158d3e9
Tyler Bridger-Griesberg <bridgergriesbergt@4268-tc423.local>
2017-12-01 09:18:43 +1100

    First Commit

---
+++ Home.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+    var price1 = x * 10;
+    document.getElementById("demo").innerHTML = price1;
+}
